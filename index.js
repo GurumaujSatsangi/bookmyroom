9232d9b379965a1966a7570da28c5630356f2d98
Gurumauj Satsangi <gurumaujsatsangi@gmail.com>
2026-06-06 17:33:34 +0530

    basic db setup and views initialized

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 import express from 'express';
 import session from 'express-session';
+import { createClient } from '@supabase/supabase-js'
+import dotenv from 'dotenv';
 
+dotenv.config();
 
 const app = express();
 
+const supabase = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_PUBLISHABLE_KEY
+)
+
 app.get("/",async(req,res)=>{
-    res.render("home.ejs");
+
+    const {data,error} = await supabase.from("hostels").select("*");
+
+    res.render("home.ejs",{hostels:data});
+})
+
+app.get("/hostel/:id",async(req,res)=>{
+    const {data,error} = await supabase.from("rooms").select("*").eq("hostel_id",req.params.id);
+    return res.render("hostel.ejs",{rooms:data});
 })
 
 app.listen("3030",()=>{
